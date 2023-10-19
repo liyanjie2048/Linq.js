e153b422e77603f8c0b8100f30262c21ce4dd687
李發財 <liyanjie2048@outlook.com>
2023-10-19 17:36:18 +0800

    Add project files.

---
+++ linq/gulpfile.js
@@ -0,0 +1,13 @@
+﻿const gulp = require('gulp');
+const rename = require('gulp-rename');
+const minifyJs = require('gulp-uglify');
+
+exports.dist = (cp) => {
+    gulp.src('./bundles/liyanjie.linq.umd.js')
+        .pipe(rename('liyanjie.linq.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifyJs())
+        .pipe(gulp.dest('./dist'));
+    cp();
+};
---
+++ linq/rollup.config.js
@@ -0,0 +1,11 @@
+﻿const typescript = require('@rollup/plugin-typescript');
+
+exports.default = {
+    input: "./src/index.ts",
+    output: {
+        file: "./bundles/liyanjie.linq.umd.js",
+        name: "liyanjie.linq",
+        format: "umd",
+    },
+    plugins: [typescript()]
+};
